7c1a9c751ff4377b7f0f092edc00f42d1d3ed82f
Shubham <shubhamnagpal30@gmail.com>
2021-05-22 19:47:47 +0530

    Fetch Min/Max bid using helper functions

--- src/Views/HomeView.jsx
+++ src/Views/HomeView.jsx
@@ -2,6 +2,29 @@ import React, { useEffect, useState } from "react";
 
 const HomeView = () => {
   const [merchantData, setMerchantData] = useState([]);
+  const [isShowMaxFirst, setIsShowMaxFirst] = useState(false);
+
+  const getMaximumBidFirst = (data) => {
+    if (data.length > 0) {
+      return data.reduce(
+        (max, value) => (value.amount > max ? value.amount : max),
+        data[0].amount
+      );
+    } else {
+      return null;
+    }
+  };
+
+  const getMinimumBidFirst = (data) => {
+    if (data.length > 0) {
+      return data.reduce(
+        (min, value) => (value.amount < min ? value.amount : min),
+        data[0].amount
+      );
+    } else {
+      return null;
+    }
+  };
 
   useEffect(() => {
     fetch("https://intense-tor-76305.herokuapp.com/merchants")
@@ -11,6 +34,9 @@ const HomeView = () => {
           return {
             id: data.id,
             avatarUrl: data.avatarUrl,
+            bidValue: isShowMaxFirst
+              ? getMaximumBidFirst(data.bids)
+              : getMinimumBidFirst(data.bids),
             email: data.email,
             name: `${data.firstname} ${data.lastname}`,
             phone: data.phone,
@@ -19,12 +45,17 @@ const HomeView = () => {
 
         setMerchantData([...merchData]);
       });
-  }, []);
+  }, [isShowMaxFirst]);
 
   return (
     <div>
       {merchantData?.map((data) => {
-        return <h1>{data.name}</h1>;
+        return (
+          <div>
+            <p>{data.name}</p>
+            <p>{data.bidValue}</p>
+          </div>
+        );
       })}
     </div>
   );
