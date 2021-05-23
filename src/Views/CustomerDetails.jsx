740204d652fc4d9a0249a620855b0adc82fe0722
Shubham <shubhamnagpal30@gmail.com>
2021-05-23 11:37:00 +0530

    Fetch User Details

--- src/Views/CustomerDetails.jsx
+++ src/Views/CustomerDetails.jsx
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 function CustomerDetails() {
+  let { id } = useParams();
+
+  useEffect(() => {
+    fetch(`https://intense-tor-76305.herokuapp.com/merchants/${id}`)
+      .then((response) => response.json())
+      .then((result) => {
+        if (Object.keys(result).length > 0) {
+          const customerDetails = result.bids.map((value) => {
+            return {
+              id: value.id,
+              carTitle: value.carTitle,
+              amount: value.amount,
+              created: value.created,
+            };
+          });
+          console.log(customerDetails);
+        }
+      });
+  }, []);
+
   return (
     <div>
       <h1>Detail View</h1>
